41758ef2854eb276f930167f5112688c02d4d60a
reliable99 <abdulmuhminsodiq@gmail.com>
2024-01-25 12:24:08 +0100

    first commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,22 @@
+// SPDX_License-identifier: MIT
+pragma solidity ^0.8.8;
+
+
+contract SimpleStorage {
+    
+    // This gets initialized to zero
+    // <- This means that this section is a comment !
+    uint256 FavoriteNumber;
+
+    function store (uint256 _favoriteNumber) public {
+        FavoriteNumber = _favoriteNumber;
+    }
+
+
+
+
+
+
+
+}
+
