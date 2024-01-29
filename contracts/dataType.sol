00df33356a88dc013fca313dc5270dfe8199269d
reliable99 <abdulmuhminsodiq@gmail.com>
2024-01-29 22:38:59 +0100

    fourth

---
+++ contracts/dataType.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.8 ;
+contract MyContract {
+    string public    stringValue = 'myValue';
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint8 public myUInt8 = 8;
+    uint256 public myUint256 = 9999;
+
+   
+}
