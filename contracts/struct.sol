bc3eaf6d6f350d77a78a03ae7ea110bda977b02f
reliable99 <abdulmuhminsodiq@gmail.com>
2024-01-30 12:46:54 +0100

    fourth

---
+++ contracts/struct.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.8 ;
+
+contract myStruct {
+    string public  value;
+
+ 
+    function set (string memory _value) public {
+        value = _value;
+    }
+}
